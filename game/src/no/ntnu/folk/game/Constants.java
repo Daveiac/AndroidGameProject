3d6bccf26d158d9e365c862cee276129362b41c7
kryel <mc@kryel.com>
2014-03-06 10:55:27 +0100

    game/ Implemented Program and added Constants Constants currently stores windowSize

---
+++ game/src/no/ntnu/folk/game/Constants.java
@@ -0,0 +1,13 @@
+package no.ntnu.folk.game;
+
+public class Constants {
+	private static int[] windowSize;
+
+	public static int[] getWindowSize() {
+		return windowSize;
+	}
+	public static void setWindowSize(int[] windowSize) {
+		Constants.windowSize = windowSize;
+	}
+
+}
--- game/src/no/ntnu/folk/game/Program.java
+++ game/src/no/ntnu/folk/game/Program.java
@@ -2,13 +2,26 @@ package no.ntnu.folk.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import no.ntnu.folk.game.states.menus.MainMenu;
+import sheep.game.Game;
 
 public class Program extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// TODO
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		Game game = new Game(this, null);
+
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+		Constants.setWindowSize(new int[]{displayMetrics.widthPixels, displayMetrics.heightPixels});
+
+		game.pushState(new MainMenu());
+		setContentView(game);
 	}
 
 }
