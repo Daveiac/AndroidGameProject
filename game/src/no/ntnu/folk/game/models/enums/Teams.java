a6f8b3dca2f5e8a5c97911882e75c4d11d66a37a
kryel <mc@kryel.com>
2014-03-06 19:15:45 +0100

    Added tagColor in Teams

--- game/src/no/ntnu/folk/game/models/enums/Teams.java
+++ game/src/no/ntnu/folk/game/models/enums/Teams.java
@@ -1,7 +1,18 @@
 package no.ntnu.folk.game.models.enums;
 
+import sheep.graphics.Color;
+
 public enum Teams {
-	BLUE,
-	RED,
-	;
+	BLUE(Color.BLUE),
+	RED(Color.RED);
+
+	private final Color tagColor;
+
+	/**
+	 * @param tagColor Color of name tag for members of this team
+	 */
+	Teams(Color tagColor) {
+		this.tagColor = tagColor;
+	}
+
 }
