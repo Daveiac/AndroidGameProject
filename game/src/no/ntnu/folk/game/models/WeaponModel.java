0e308f1b87efa13b9528dd550f92efd5abe31183
kryel <mc@kryel.com>
2014-03-06 17:51:40 +0100

    Added javadoc to models and enums for Projectile and Weapon

--- game/src/no/ntnu/folk/game/models/WeaponModel.java
+++ game/src/no/ntnu/folk/game/models/WeaponModel.java
@@ -5,10 +5,22 @@ import no.ntnu.folk.game.models.enums.Weapons;
 public class WeaponModel {
 	private final ProjectileModel projectile;
 
+	/**
+	 * Constructing a weaponModel based on a weapon from Weapons
+	 *
+	 * @param weapon data for this weapon
+	 */
 	public WeaponModel(Weapons weapon) {
 		this.projectile = new ProjectileModel(weapon.getProjectile());
 	}
 
 	// TODO
+
+	/**
+	 * @return this weapon's projectile
+	 */
+	public ProjectileModel getProjectile() {
+		return projectile;
+	}
 
 }
