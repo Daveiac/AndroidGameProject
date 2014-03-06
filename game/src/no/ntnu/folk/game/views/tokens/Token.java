044d78b30e6dcf812a5f1ab7b257608f8ebe0bb8
kryel <mc@kryel.com>
2014-03-06 19:00:22 +0100

    Overriding update and draw in Token

--- game/src/no/ntnu/folk/game/views/tokens/Token.java
+++ game/src/no/ntnu/folk/game/views/tokens/Token.java
@@ -1,7 +1,17 @@
 package no.ntnu.folk.game.views.tokens;
 
+import android.graphics.Canvas;
 import sheep.game.Sprite;
 
 public abstract class Token extends Sprite {
-	// TODO
+
+	@Override
+	public void update(float dt) {
+		super.update(dt);  // TODO
+	}
+	@Override
+	public void draw(Canvas canvas) {
+		super.draw(canvas);  // TODO
+	}
+
 }
