0c5ff88ba20d28866a0e067cfea83ad53a9503db
Rune <rune.abrahamsen@hotmail.com>
2014-03-07 11:51:57 +0100

    Added game states as constatins
    Updated the relevant classes, and added Int constatins for states of the
    game

--- game/src/no/ntnu/folk/game/states/menus/LoadMenu.java
+++ game/src/no/ntnu/folk/game/states/menus/LoadMenu.java
@@ -1,26 +1,25 @@
 package no.ntnu.folk.game.states.menus;
 
+import no.ntnu.folk.game.Constants;
 import no.ntnu.folk.game.states.GameState;
 import no.ntnu.folk.game.states.MenuState;
 
 public class LoadMenu extends MenuState {
-	public static final int LOAD_GAME = 3;
-	public static final int BACK = 4;
 	// TODO
 
 	@Override
 	protected void addMenuItems() {
 		menuItems = new MenuItem[2];
-		menuItems[0] = new MenuItem(LOAD_GAME, 0, 100, 0, 100);
-		menuItems[1] = new MenuItem(BACK, 100, 200, 0, 100);
+		menuItems[0] = new MenuItem(Constants.LOAD_GAME, 0, 100, 0, 100);
+		menuItems[1] = new MenuItem(Constants.BACK, 100, 200, 0, 100);
 	}
 	@Override
 	protected void clickMenuItem(MenuItem menuItem) {
 		switch (menuItem.getLabel()) {
-			case LOAD_GAME:  // TODO
+			case Constants.LOAD_GAME:  // TODO
 				getGame().pushState(new GameState());
 				break;
-			case BACK:
+			case Constants.BACK:
 				getGame().popState();
 				break;
 			default:
--- game/src/no/ntnu/folk/game/states/menus/MainMenu.java
+++ game/src/no/ntnu/folk/game/states/menus/MainMenu.java
@@ -1,32 +1,30 @@
 package no.ntnu.folk.game.states.menus;
 
+import no.ntnu.folk.game.Constants;
 import no.ntnu.folk.game.states.GameState;
 import no.ntnu.folk.game.states.MenuState;
 
 public class MainMenu extends MenuState {
-	public static final int START_GAME = 0;
-	public static final int PRE_GAME_MENU = 1;
-	public static final int LOAD_MENU = 2;
 
 	// TODO
 
 	@Override
 	protected void addMenuItems() {
 		menuItems = new MenuItem[3];
-		menuItems[0] = new MenuItem(START_GAME, 0, 100, 0, 100);
-		menuItems[1] = new MenuItem(PRE_GAME_MENU, 100, 200, 0, 100);
-		menuItems[2] = new MenuItem(LOAD_MENU, 200, 300, 0, 100);
+		menuItems[0] = new MenuItem(Constants.START_GAME, 0, 100, 0, 100);
+		menuItems[1] = new MenuItem(Constants.PRE_GAME_MENU, 100, 200, 0, 100);
+		menuItems[2] = new MenuItem(Constants.LOAD_MENU, 200, 300, 0, 100);
 	}
 	@Override
 	protected void clickMenuItem(MenuItem menuItem) {
 		switch (menuItem.getLabel()) {
-			case START_GAME:
+			case Constants.START_GAME:
 				getGame().pushState(new GameState());
 				break;
-			case PRE_GAME_MENU:
+			case Constants.PRE_GAME_MENU:
 				getGame().pushState(new PreGameMenu());
 				break;
-			case LOAD_MENU:
+			case Constants.LOAD_MENU:
 				getGame().pushState(new LoadMenu());
 				break;
 			default:
