8ea7b9b936f183938b5e321b559792208508c611
Rune <rune.abrahamsen@hotmail.com>
2014-03-07 13:04:16 +0100

    PreGameMenu
    Changed to work with current system

--- game/src/no/ntnu/folk/game/states/menus/PreGameMenu.java
+++ game/src/no/ntnu/folk/game/states/menus/PreGameMenu.java
@@ -1,26 +1,25 @@
 package no.ntnu.folk.game.states.menus;
 
+import no.ntnu.folk.game.Constants;
 import no.ntnu.folk.game.states.GameState;
 import no.ntnu.folk.game.states.MenuState;
 
 public class PreGameMenu extends MenuState {
-	public static final int START_GAME = 0;
-	public static final int BACK = 4;
 	// TODO
 
 	@Override
 	protected void addMenuItems() {
 		menuItems = new MenuItem[2];
-		menuItems[0] = new MenuItem(START_GAME,"Start game", 0, 100, 0, 100);
-		menuItems[1] = new MenuItem(BACK,"Back", 100, 200, 0, 100);
+		menuItems[0] = new MenuItem(Constants.START_GAME,"Start game", 0, 100, 0, 100);
+		menuItems[1] = new MenuItem(Constants.BACK,"Back", 100, 200, 0, 100);
 	}
 	@Override
 	protected void clickMenuItem(MenuItem menuItem) {
 		switch (menuItem.getState()) {
-			case START_GAME:
+			case Constants.START_GAME:
 				getGame().pushState(new GameState());
 				break;
-			case BACK:
+			case Constants.BACK:
 				getGame().popState();
 				break;
 			default:
