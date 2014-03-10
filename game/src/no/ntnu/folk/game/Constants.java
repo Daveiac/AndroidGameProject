c34b0d5154963fc06e3b88fd358b903c808a14e5
shiminsun <small_monster1992@hotmail.com>
2014-03-10 15:02:08 +0100

    Sync back to gameplay

--- game/src/no/ntnu/folk/game/Constants.java
+++ game/src/no/ntnu/folk/game/Constants.java
@@ -3,10 +3,14 @@ package no.ntnu.folk.game;
 public class Constants {
 	// Debugging
 	public static boolean debugging = true;
-
+	// Game options
+	public static final int DEFAULT_HEALTH = 100;
+	public static final int MAX_HEALTH = 1000;
+	public static String[] LEVEL_LIST = {"1","2"}; //TODO make level list
+	public static String[] GAME_TYPE = {"FFA", "TEAM"};
 	// Player count
 	public static final int MAX_PLAYERS = 6;
-	public static final int DEFAULT_PLAYER_COUNT = 1;
+	public static final int DEFAULT_PLAYER_COUNT = 2;
 
 	// Window size
 	private static int[] windowSize;
