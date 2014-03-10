fa7dff76ba2ec92c2ac636260813910b2f6ff952
frank-albert <cspram90@gmail.com>
2014-03-10 12:13:49 +0100

    created res/levels/test.level
    levelParser - done something (sry)

--- game/src/no/ntnu/folk/game/levels/levelParser.java
+++ game/src/no/ntnu/folk/game/levels/levelParser.java
@@ -3,6 +3,7 @@ package no.ntnu.folk.game.levels;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
@@ -12,27 +13,30 @@ import no.ntnu.folk.game.views.tokens.Token;
 
 public class levelParser {
 
-	
+
 	public static ArrayList<Token> parseLevel(String levelName) {
-		
+
 		ArrayList<Token> tokens = new ArrayList<Token>();
 
 		String path = "res/levels/" + levelName + ".level";
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(path));
-			
+
 			String line = br.readLine();
-			
 			while (line != null) {
 				if (line.length() != 0) {
 					tokens.add(parseLine(line));
 				}
+				line = br.readLine();
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+
 		return tokens;
 	}
 
