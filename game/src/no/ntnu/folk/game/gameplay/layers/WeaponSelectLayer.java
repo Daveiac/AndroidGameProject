95f3ac646e980fce46646b00803cc39f21dad9c2
Rune <rune.abrahamsen@hotmail.com>
2014-03-12 11:23:11 +0100

    Added a WeaponSelectLayer

---
+++ game/src/no/ntnu/folk/game/gameplay/layers/WeaponSelectLayer.java
@@ -0,0 +1,26 @@
+package no.ntnu.folk.game.gameplay.layers;
+
+import no.ntnu.folk.game.gameplay.models.GameModel;
+import android.graphics.Canvas;
+import sheep.game.Layer;
+import sheep.math.BoundingBox;
+
+public class WeaponSelectLayer extends Layer {
+
+	public WeaponSelectLayer(GameModel model) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Canvas canvas, BoundingBox box) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
