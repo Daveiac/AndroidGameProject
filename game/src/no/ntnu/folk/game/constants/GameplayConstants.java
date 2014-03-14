4293302a3ce33753f8c0306c9f617a699d71655b
shiminsun <small_monster1992@hotmail.com>
2014-03-14 14:29:55 +0100

    added BULLET_SPEED as a constant

--- game/src/no/ntnu/folk/game/constants/GameplayConstants.java
+++ game/src/no/ntnu/folk/game/constants/GameplayConstants.java
@@ -12,4 +12,6 @@ public class GameplayConstants {
 	public static final int DEFAULT_PLAYER_COUNT = 2;
 	// TurnTime
 	public static final int TURN_TIME = 10; // turn time in seconds
+	// Projectile speed
+	public static final int BULLET_SPEED = 50; // bullet speed for handgun
 }
