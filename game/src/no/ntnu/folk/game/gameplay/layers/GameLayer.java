0358bf89a20ad1158b56ae17f71c760aa071fef5
Daveiac <daveiac@hotmail.com>
2014-03-21 10:59:12 +0100

    started working with collisions. Currently just a sysout when the player hits something.

--- game/src/no/ntnu/folk/game/gameplay/layers/GameLayer.java
+++ game/src/no/ntnu/folk/game/gameplay/layers/GameLayer.java
@@ -1,6 +1,6 @@
 package no.ntnu.folk.game.gameplay.layers;
 
-import android.graphics.Canvas;
+import no.ntnu.folk.game.constants.GameplayConstants;
 import no.ntnu.folk.game.constants.ProgramConstants;
 import no.ntnu.folk.game.gameplay.entities.models.PlayerModel;
 import no.ntnu.folk.game.gameplay.entities.models.ProjectileModel;
@@ -10,6 +10,7 @@ import no.ntnu.folk.game.gameplay.models.GameModel;
 import sheep.game.Layer;
 import sheep.graphics.Color;
 import sheep.math.BoundingBox;
+import android.graphics.Canvas;
 
 public class GameLayer extends Layer {
 	private GameModel model;
@@ -23,7 +24,7 @@ public class GameLayer extends Layer {
 
 	@Override
 	public void update(float dt) {
-		// Do nothing
+		collidesWithWall();
 	}
 
 	@Override
@@ -53,6 +54,53 @@ public class GameLayer extends Layer {
 				windowSize[1] * 0.1f,
 				Color.WHITE
 		);
+	}
+	
+	private boolean collidesWithWall() {
+		PlayerModel player = this.model.getCurrentPlayer();
+		int gridSize = GameplayConstants.GRID_SIZE;
+		float x = player.getX();
+		float y = player.getY();
+		float offX = -player.getOffset().getX();
+		float offY = -player.getOffset().getY();
+		int[] topLeft = {(int)(x-offX)/gridSize, (int)(y-offY)/gridSize};
+		int[] topRight = {(int)(x+offX)/gridSize, (int)(y-offY)/gridSize};
+		int[] bottomLeft = {(int)(x-offX)/gridSize, (int)(y+offY)/gridSize};
+		int[] bottomRight = {(int)(x+offX)/gridSize, (int)(y+offY)/gridSize};
+		LevelToken[][] grid = model.getCurrentLevel().getGrid();
+		if(grid == null) {
+			System.out.println("grid is null");
+			return false;
+		}
+		//left wall
+		for (int i = topLeft[1]; i < bottomLeft[1]+1; i++) {
+			if(grid[i][topLeft[0]] != null) {
+				System.out.println("player hitting left wall\n");
+				return true;
+			}
+		}
+		//right wall
+		for (int i = topRight[1]; i < bottomRight[1]+1; i++) {
+			if(grid[i][topRight[0]] != null) {
+				System.out.println("player hittingc right wall\n");
+				return true;
+			}
+		}
+		//floor
+		for (int i = bottomLeft[1]; i < bottomRight[1]+1; i++) {
+			if(grid[bottomLeft[1]][i] != null) {
+				System.out.println("player hitting floor\n");
+				return true;
+			}
+		}
+		//ceiling
+		for (int i = topLeft[1]; i < topRight[1]+1; i++) {
+			if(grid[topLeft[1]][i] != null) {
+				System.out.println("player hitting ceiling\n");
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
