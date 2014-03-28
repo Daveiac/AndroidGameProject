5cac28410b1f8ae84892ee500ae291748f114eb0
kryel <mc@kryel.com>
2014-03-28 17:04:50 +0100

    Refactoring + javadoc for calculateHealthBarColor

--- game/src/no/ntnu/folk/game/gameplay/entities/views/PlayerToken.java
+++ game/src/no/ntnu/folk/game/gameplay/entities/views/PlayerToken.java
@@ -41,9 +41,13 @@ public class PlayerToken extends EntityToken {
 		healthBarMatrix.setTranslate(x, y - height);
 		float health = ((PlayerModel) entityModel).getHealth() * 1.0f / ((PlayerModel) entityModel).getStartHealth();
 		canvas.drawRect(x - width - healthBarFrame, y - height + healthBarFrame, x + width + healthBarFrame, y - height * (1.25f) - healthBarFrame, Color.BLACK);
-		canvas.drawRect(x - width, y - height, (x - width) + width * health * 2, y - height * (1.25f), getPaint(health));
+		canvas.drawRect(x - width, y - height, (x - width) + width * health * 2, y - height * (1.25f), calculateHealthBarColor(health));
 	}
-	private Color getPaint(float health) {
+	/**
+	 * @param health Player health
+	 * @return The color the health bar should be drawn
+	 */
+	private Color calculateHealthBarColor(float health) {
 		int red = health < 0.5f ? 255 : (int) (255 - 2 * health * 255);
 		int green = health > 0.5f ? 255 : (int) (2 * health * 255);
 		int blue = 0;
