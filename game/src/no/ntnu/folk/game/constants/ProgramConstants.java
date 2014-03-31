8052a61882fd46d37ae6b7807f976cc4c96ca3a9
frank-albert <cspram90@gmail.com>
2014-03-31 11:52:49 +0200

    javadoc ProgramConstants

--- game/src/no/ntnu/folk/game/constants/ProgramConstants.java
+++ game/src/no/ntnu/folk/game/constants/ProgramConstants.java
@@ -21,37 +21,57 @@ public class ProgramConstants {
 		ProgramConstants.buttonHeight = windowSize[1] / 8;
 	}
 
+	/**
+	 * @return The button width
+	 */
 	public static int getButtonWidth() {
 		return buttonWidth;
 	}
+
+	/**
+	 * @return The button height
+	 */
 	public static int getButtonHeight() {
 		return buttonHeight;
 	}
 
+	/**
+	 * @return True if debugging is on, else return false if it is off.
+	 */
 	public static boolean isDebugging() {
 		return debugging;
 	}
+
+	/**
+	 * Toggles the debugging option to either on or off depending on the previous condition.
+	 */
 	public static void toggleDebugging() {
 		debugging = !debugging;
 	}
 
+	/**
+	 * @return True if unlimited fire option is on, else return false if it is not.
+	 */
 	public static boolean isUnlimitedFire() {
 		return unlimitedFire;
 	}
 	
 	/**
-	 * This method toggles the unlimited fire option on or off when debugging.
+	 * Toggles the unlimited fire option to either on or off depending on the previous condition when debugging.
 	 */
 	public static void toggleUnlimitedFire() {
 		unlimitedFire = !unlimitedFire;
 	}
-	
+
+	/**
+	 * @return True if debugging walls option is on, else return false if it is not.
+	 */
 	public static boolean isDebugWalls() {
 		return debugWalls;
 	}
 	
 	/**
-	 * This method toggles the option for debugging walls on and off when debugging.
+	 * Toggles the debugging walls option to either on or off depending on the previous condition when debugging.
 	 */
 	public static void toggleDebugWalls() {
 		debugWalls = !debugWalls;
