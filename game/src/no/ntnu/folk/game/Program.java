94aed8cf2d2b2e520e1fe5bf71a8b3202363b7cd
Rune <rune.abrahamsen@hotmail.com>
2014-03-31 12:13:21 +0200

    nothing

--- game/src/no/ntnu/folk/game/Program.java
+++ game/src/no/ntnu/folk/game/Program.java
@@ -36,8 +36,8 @@ public class Program extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		storeWindowSize();
 		initializeGame();
